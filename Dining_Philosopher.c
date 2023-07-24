5691a4d0027bc8b1e0ce465126b623dbd88cca6d
Gocode277 <137083457+Gocode277@users.noreply.github.com>
2023-07-24 12:42:13 +0530

    Dining_Philosopher.c

--- Dining_Philosopher.c
+++ Dining_Philosopher.c
@@ -15,36 +15,58 @@ int phil[N] = { 0, 1, 2, 3, 4 };
 sem_t mutex;
 sem_t S[N];
 
-#define MAX_ITERATIONS 5 // Set a fixed number of iterations
+void test(int phnum)
+{
+    if (state[phnum] == HUNGRY
+        && state[LEFT] != EATING
+        && state[RIGHT] != EATING) {
 
-void test(int phnum) {
-    if (state[phnum] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING) {
         state[phnum] = EATING;
 
-        printf("Philosopher %d takes fork %d and %d\n", phnum + 1, LEFT + 1, phnum + 1);
+
+
+        printf("Philosopher %d takes fork %d and %d\n",
+                      phnum + 1, LEFT + 1, phnum + 1);
+
         printf("Philosopher %d is Eating\n", phnum + 1);
 
         sem_post(&S[phnum]);
     }
 }
 
-void take_fork(int phnum) {
+
+void take_fork(int phnum)
+{
+
     sem_wait(&mutex);
 
+
     state[phnum] = HUNGRY;
+
     printf("Philosopher %d is Hungry\n", phnum + 1);
+
 
     test(phnum);
 
     sem_post(&mutex);
+
+
     sem_wait(&S[phnum]);
+
+
 }
 
-void put_fork(int phnum) {
+
+void put_fork(int phnum)
+{
+
     sem_wait(&mutex);
 
+
     state[phnum] = THINKING;
-    printf("Philosopher %d putting fork %d and %d down\n", phnum + 1, LEFT + 1, phnum + 1);
+
+    printf("Philosopher %d putting fork %d and %d down\n",
+           phnum + 1, LEFT + 1, phnum + 1);
     printf("Philosopher %d is thinking\n", phnum + 1);
 
     test(LEFT);
@@ -53,33 +75,44 @@ void put_fork(int phnum) {
     sem_post(&mutex);
 }
 
-void* philosopher(void* num) {
-    int* i = num;
-    int iteration = 0;
+void* philosopher(void* num)
+{
 
-    while (iteration < MAX_ITERATIONS) { // Termination condition
+    while (1) {
+
+        int* i = num;
+
+
+
         take_fork(*i);
+
+
         put_fork(*i);
-        iteration++;
     }
 }
 
-int main() {
+int main()
+{
+
     int i;
     pthread_t thread_id[N];
+
 
     sem_init(&mutex, 0, 1);
 
     for (i = 0; i < N; i++)
+
         sem_init(&S[i], 0, 0);
 
     for (i = 0; i < N; i++) {
-        pthread_create(&thread_id[i], NULL, philosopher, &phil[i]);
+
+        pthread_create(&thread_id[i], NULL,
+                       philosopher, &phil[i]);
+
         printf("Philosopher %d is thinking\n", i + 1);
     }
 
     for (i = 0; i < N; i++)
-        pthread_join(thread_id[i], NULL);
 
-    return 0;
+        pthread_join(thread_id[i], NULL);
 }
